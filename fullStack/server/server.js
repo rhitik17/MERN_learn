6a405afe16c951e5ef034f79e6d7f449ea436e7c
Rhitik_HBR <ritikbhandari17@gmail.com>
2024-12-11 16:01:00 +0545

    feat:Added dotenv, morgan, and updated package-lock.json and package.json to reflect new dependencies

--- fullStack/server/server.js
+++ fullStack/server/server.js
@@ -1,16 +1,30 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
 const corsOption = {
-    origin: ["http://localhost:5173"],
+  origin: ["http://localhost:5173"],
 }
+const morgan = require("morgan");
+const dotenv = require("dotenv");
 
+const app = express();
+
+// dotenv configuration 
+dotenv.config();
+
+//middleware
 app.use(cors(corsOption));
+app.use(express.json());
+app.use(morgan("dev"))
 
+
+// route
 app.get("/fruits", (req, res) => {
   res.json({ fruits: ["apple", "banana", "strawberry"] });
 });
 
-app.listen(8080, ()=>{
- console.log("server started on port 8080")
+// port
+const PORT =process.env.PORT || 8080;
+
+app.listen(PORT, ()=>{
+ console.log(`server started on port ${PORT}`);
 })
